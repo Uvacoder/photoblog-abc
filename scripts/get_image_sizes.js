de074e2600663f05df6bec44df36c0c79acf472e
Moritz L'Hoest <info@herebedragons.world>
2020-12-30 17:23:20 +0100

    Add precommit hook for image sizes

--- scripts/get_image_sizes.js
+++ scripts/get_image_sizes.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const image_size = require('image-size');
-const { stdout } = require('process');
 
 /**
  * Get a flat list of all files in a directory. Searches subdirectories recursively.
@@ -16,13 +15,34 @@ const listFilesInDirectory = (dir, basePath) => fs.readdirSync(dir, { withFileTy
         )
     .flat();
 
+/**
+ * Get a map of image dimensions for the passed files.
+ * @param string dir    Base directory (file names should be relative to it).
+ * @param array files   Array of filenames.
+ */
 const getDimensions = (dir, files) => Object.fromEntries(
     files.map(file => [file, image_size(path.resolve(dir, file))])
 );
 
-
+// directory with photos and target file
 const photoDirectory = path.resolve(__dirname, '../site/photos/');
+const targetFile = path.resolve(__dirname, '../site/_data/image_sizes.json');
+
+// get list of files and their dimensions
 const photos = listFilesInDirectory(photoDirectory, photoDirectory);
 const photoInfo = getDimensions(photoDirectory, photos);
+const encodedInfo = JSON.stringify(photoInfo);
 
-stdout.write(JSON.stringify(photoInfo));
+// get the current file contents
+const currentContents = fs.readFileSync(targetFile, 'utf8');
+
+// overwrite the file with the new information
+fs.writeFileSync(targetFile, encodedInfo, { encoding: 'utf8' });
+
+// if instructed, exit with error code if the file has changed (for usage in pre-commit hook)
+const throwOnDiff = process.argv.includes('--throw-on-diff');
+if (throwOnDiff && (currentContents !== encodedInfo)) {
+    process.exit(1);
+}
+
+process.exit(0);
